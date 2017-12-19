3eb4bcbd711876148706c71de1219dbbd6f4a6ab
Tamizhvendan S <tamizh88@gmail.com>
2017-12-19 10:25:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  // Map of hooks
+  hooks: {},
+
+  // Map of new blocks
+  blocks: {
+    pel : {
+      process : function(block) {
+        var emptyLineCount = block.args[0] || 1;
+        var i = 1;
+        var body = ""
+        while (i <= emptyLineCount) {
+          body += "</br> "
+        }
+        return body;
+      }
+    }
+  },
+
+  // Map of new filters
+  filters: {}
+};
